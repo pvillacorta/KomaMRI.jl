69fa0897ec50ae393239739279c08df831363e89
Pablo Villacorta <pablo.villacorta@uva.es>
2024-09-11 13:22:50 +0200

    Remove `unit_time` extend

--- KomaMRICore/ext/KomaoneAPIExt.jl
+++ KomaMRICore/ext/KomaoneAPIExt.jl
@@ -63,29 +63,4 @@ function __init__()
     @warn "oneAPI does not support all array operations used by KomaMRI. GPU performance may be slower than expected"
 end
 
-const AdjointOneArray{T, N, M} = LinearAlgebra.Adjoint{T, oneArray{T, N, M}} where {T<:Real, N, M}
-## Extend KomaMRIBase.unit_time (until bug with oneAPI is solved)
-function KomaMRIBase.unit_time(t::AdjointOneArray{T, N, M}, ts::KomaMRIBase.TimeRange{T}) where {T<:Real, N, M}
-    # tmp = (t .- ts.t_start) ./ (ts.t_end - ts.t_start)
-    # mask = (tmp .> zero(T))
-    # tmp .*= mask
-
-    # tmp = max.((t .- ts.t_start) ./ (ts.t_end - ts.t_start), zero(T))
-
-    # _ = sum(t)
-
-    # mask .= (tmp .<= oneunit(T))
-    # return tmp .* mask .+ .!mask
-
-    # t = min.(tmp, oneunit(T))
-
-    # _ = sum(t)
-
-    t_unit = KomaMRIBase._unit_time(t, ts)
-
-    # KA.synchronize(KA.get_backend(t))
-
-    return t_unit
-end
-
 end
